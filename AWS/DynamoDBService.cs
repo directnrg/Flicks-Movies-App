402f83c60d4f1770d721d08b99cb5ebdb5f41960
Yiseul Ko <gls20020128@gmail.com>
2023-10-20 22:12:22 -0400

    Incomplete movie search

---
+++ AWS/DynamoDBService.cs
@@ -0,0 +1,54 @@
+﻿using _301153142_301137955_Soto_Ko_Lab3.Models;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+
+namespace _301153142_301137955_Soto_Ko_Lab3.AWS
+{
+    public static class DynamoDBService
+    {
+        static DynamoDBContext context;
+        /*
+         * get the list of movie meta data items
+         */
+        public static async Task<List<MovieModel>> GetAllMovies()
+        {
+            var request = new QueryRequest
+            {
+                TableName = "YourTableName",
+                FilterExpression = "attribute_exists(title) OR size(title) > 0",
+            };
+
+            QueryResponse res = await AWSClients.dynamoClient.QueryAsync(request);
+
+            List<MovieModel> movies = new ();
+
+            foreach (var m in res.Items)
+            {
+                MovieModel movie = CreateMovieFromItem(m);
+
+                movies.Add(movie);
+            }
+            return movies;
+        }
+
+
+        private static MovieModel CreateMovieFromItem(Dictionary<string, AttributeValue> movieItem)
+        {
+            return new MovieModel()
+            {
+                MovieId = movieItem["MovieId"].S,
+                UserId = movieItem["UserId"].S,
+                Title = movieItem["Title"].S,
+                Genre = movieItem["Genre"].S,
+                Directors = movieItem["Directors"].SS,
+                AvgRating =  double.Parse(movieItem["AvgRatings"].N),
+                NumOfRatings = int.Parse(movieItem["NumOfRatings"].N),
+                VideoS3Key = movieItem["VideoS3Key"].S,
+                ThumbnailS3Key = movieItem["ThumbnailS3Key"].S,
+            };
+            
+        }
+
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,95 @@
+﻿using _301153142_301137955_Soto_Ko_Lab3.Areas.Identity.Data;
+using _301153142_301137955_Soto_Ko_Lab3.AWS;
+using _301153142_301137955_Soto_Ko_Lab3.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _301153142_301137955_Soto_Ko_Lab3.Controllers
+{
+    [Authorize]
+    public class MovieController : Controller
+    {
+        private readonly UserManager<CustomUser> _userManager;
+        public List<MovieModel> Movies { get; set; }
+
+        // GET: Movie
+        public async Task<ActionResult> Index()
+        {
+            Movies = new List<MovieModel>();
+            //Movies = await DynamoDBService.GetAllMovies();
+
+            return View(Movies);
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
