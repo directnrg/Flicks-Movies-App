fb5012ade200d72c618c6fb55f043faae844037f
Fabian Soto <directnrg@gmail.com>
2023-10-28 21:55:59 -0400

    Adding and getting comments and ratings base logic, base UI for rating and displaying comments.

--- Models/Movie/DetailsViewModel.cs
+++ Models/Movie/DetailsViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace _301153142_301137955_Soto_Ko_Lab3.Models.Movie
+﻿using _301153142_301137955_Soto_Ko_Lab3.Models.Review;
+
+namespace _301153142_301137955_Soto_Ko_Lab3.Models.Movie
 {
     public class DetailsViewModel
     {
-        public MovieModel Movie { get; set; }
+        public MovieModel? Movie { get; set; }
+
+        public ReviewViewModel ReviewViewModel { get; set; } = new();
+
+        public string Comment { get; set; } = string.Empty;
+        public double Rating { get; set; } = 0;
     }
 }
