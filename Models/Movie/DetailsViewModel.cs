40135747de78cffaf01ab11dc973a255e1a15c03
Fabian Soto <directnrg@gmail.com>
2023-10-30 13:55:13 -0400

    Completed implementation of adding ratings and comments. fixed UI to show buttons to edit and pass comment and rating to modal

--- Models/Movie/DetailsViewModel.cs
+++ Models/Movie/DetailsViewModel.cs
@@ -10,7 +10,7 @@ namespace _301153142_301137955_Soto_Ko_Lab3.Models.Movie
         public ReviewViewModel ReviewViewModel { get; set; } = new();
 
         public string Comment { get; set; } = string.Empty;
-        public double Rating { get; set; } = 1;
+        public double Rating { get; set; } = 0;
         public bool IsAddBtnHidden { get; set; } = false;
     }
 }
