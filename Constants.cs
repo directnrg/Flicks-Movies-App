75f1f5795c5cb43f9ae31ddfc6f7bad767a8e3fe
Fabian Soto <directnrg@gmail.com>
2023-10-31 00:55:44 -0400

    refactor how data is passed - now using session for ignored properties. updated movieController GetAction into separate actions to separate concerns following SRP

--- Constants.cs
+++ Constants.cs
@@ -2,53 +2,57 @@
 {
     public static class Constants
     {
-        public static string PREFIX_MOVIE = "MOVIE#";
-        public static string PREFIX_COMMENT = "COMMENT#";
-        public static string PREFIX_RATING = "RATING#";
-        public static string MOVIE_ID = "MovieId";
-        public static string USER_ID = "UserId";
-        public static string TIMESTAMP = "Timestamp";
-        public static string TYPE = "Type";
-        public static string GENRE = "Genre";
-        public static string AVG_RATING = "AvgRating";
-        public static string TITLE = "Title";
-        public static string VIDEO = "Video";
-        public static string IMAGE = "image";
+        public static readonly string PREFIX_MOVIE = "MOVIE#";
+        public static readonly string PREFIX_COMMENT = "COMMENT#";
+        public static readonly string PREFIX_RATING = "RATING#";
+        public static readonly string MOVIE_ID = "MovieId";
+        public static readonly string USER_ID = "UserId";
+        public static readonly string TIMESTAMP = "Timestamp";
+        public static readonly string TYPE = "Type";
+        public static readonly string GENRE = "Genre";
+        public static readonly string AVG_RATING = "AvgRating";
+        public static readonly string TITLE = "Title";
+        public static readonly string VIDEO = "Video";
+        public static readonly string IMAGE = "image";
 
-        public static char FORWARD_SLASH = '/';
-        public static char COMMA = ',';
-        public static string COMMA_DELIMITER = ", ";
-        public static string PERIOD = ".";
-        public static string HASHTAG = "#";
+        public static readonly char FORWARD_SLASH = '/';
+        public static readonly char COMMA = ',';
+        public static readonly string COMMA_DELIMITER = ", ";
+        public static readonly string PERIOD = ".";
+        public static readonly string HASHTAG = "#";
 
         // msg related
-        public static string SUCCESS = "SUCCESS";
-        public static string ERROR = "ERROR";
-        public static string NOT_AUTHORIZED_MSG = "This action is not authorized for this user.";
+        public static readonly string SUCCESS = "SUCCESS";
+        public static readonly string ERROR = "ERROR";
+        public static readonly string NOT_AUTHORIZED_MSG = "This action is not authorized for this user.";
 
         // bucket name
-        public static string MOVIE_BUCKET_NAME = "comp306-lab3-movies-sk";
-        public static string VIDEO_FOLDER = "movies/";
-        public static string THUMBNAIL_FOLDER = "thumbnails/";
+        public static readonly string MOVIE_BUCKET_NAME = "comp306-lab3-movies-sk";
+        public static readonly string VIDEO_FOLDER = "movies/";
+        public static readonly string THUMBNAIL_FOLDER = "thumbnails/";
 
         // GSI
-        public static string GSI_GENRE = "Genre-index";
-        public static string GSI_AVG_RATING = "AvgRating-index";
-        public static string GSI_TYPE_TIMESTAMP = "Type-Timestamp-index";
-        public static string GSI_USER_MOVIE = "UserId-MovieId-index";
+        public static readonly string GSI_GENRE = "Genre-index";
+        public static readonly string GSI_AVG_RATING = "AvgRating-index";
+        public static readonly string GSI_TYPE_TIMESTAMP = "Type-Timestamp-index";
+        public static readonly string GSI_USER_MOVIE = "UserId-MovieId-index";
 
         //VIEWS 
-        public static string VIEW_DETAILS = "Details";
-        public static string VIEW_ERROR = "Error";
-        public static string VIEW_UPDATE = "Update";
+        public static readonly string VIEW_DETAILS = "Details";
+        public static readonly string VIEW_ERROR = "Error";
+        public static readonly string VIEW_UPDATE = "Update";
 
         //TempData
-        public static string TEMP_MOVIE = "Movie";
-        public static string TEMP_COMMENTS = "Comments";
-        public static string TEMP_RATINGS = "Ratings";
+        public static readonly string TEMP_MOVIE = "Movie";
+        public static readonly string TEMP_COMMENTS = "Comments";
+        public static readonly string TEMP_RATINGS = "Ratings";
 
         // action
-        public static string UPDATE = "update";
-        public static string DELETE = "delete";
+        public static readonly string UPDATE = "update";
+        public static readonly string DELETE = "delete";
+
+        //
+        public static readonly string SESSION_THUMB64 = "ThumbnailBase64";
+        public static readonly string SESSION_CONTENT_TYPE = "ThumbnailContentType";
     }
 }
